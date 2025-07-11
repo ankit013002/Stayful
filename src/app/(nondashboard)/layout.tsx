2483b18f66f729a1021e117e61815c44d01ddace
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-07-11 15:46:14 -0400

    Added ability to store user data in the database and also fetch the data. Simply create user if they are signed in via cognito or grab user if they already exist.

--- src/app/(nondashboard)/layout.tsx
+++ src/app/(nondashboard)/layout.tsx
@@ -1,17 +1,18 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { useGetAuthUserQuery } from "@/state/api";
 import React, { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
+  const { data: authUser } = useGetAuthUserQuery();
+  console.log("authUser:", authUser);
+
   return (
     <div className="h-full w-full">
       <Navbar />
-      <main
-        className={`h-full w-full flex flex-col`}
-        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
-      >
-        {children}
-      </main>
+      <main className={`h-full w-full flex flex-col`}>{children}</main>
     </div>
   );
 };
